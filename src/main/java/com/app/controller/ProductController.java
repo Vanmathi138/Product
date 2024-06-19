2455b9bdcd86ba04385ee53f4ac5c9e0ade5731c
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2024-06-19 17:25:42 +0530

    pagination task

--- src/main/java/com/app/controller/ProductController.java
+++ src/main/java/com/app/controller/ProductController.java
@@ -1,7 +1,17 @@
 package com.app.controller;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.dto.APIResponse;
+import com.app.entity.Product;
 import com.app.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -11,6 +21,30 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api")
 public class ProductController {
 	private final ProductService service;
-
+	private final PagedResourcesAssembler<Product> pagedResource;
+	
+	@GetMapping("/getAll")
+	public APIResponse<List<Product>> getAll(){
+		List<Product> allProducts = service.getAllProducts();
+		return new APIResponse<>(allProducts.size(),allProducts);
+	}
+	@GetMapping("/get-{field}")
+	public APIResponse<List<Product>> getByField(@PathVariable String field){
+		List<Product> product = service.findByField(field);
+		return new APIResponse<>(product.size(),product);
+	}
+	@GetMapping("/pagination/{offset}/{pageSize}")
+	public APIResponse<PagedModel<?>> getProduct(@PathVariable int offset, @PathVariable int pageSize){
+		Page<Product> allProduct = service.findProductBySize(offset,pageSize);
+		PagedModel<?> pagedModel = pagedResource.toModel(allProduct);
+		return new APIResponse<>(allProduct.getSize(),pagedModel);
+	}
+	@GetMapping("/paginationWithSort/{offset}/{pageSize}/{field}")
+	public APIResponse<PagedModel<?>> getPaginationWithSort(@PathVariable int offset,
+			@PathVariable int pageSize, @PathVariable String field){
+		Page<Product> product = service.findPaginationWithSort(offset,pageSize,field);
+		PagedModel<?> getProduct = pagedResource.toModel(product);
+		return new APIResponse<>(product.getSize(),getProduct);
+	}
 
 }
--- src/main/java/com/app/service/ProductService.java
+++ src/main/java/com/app/service/ProductService.java
@@ -5,6 +5,10 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.app.entity.Product;
@@ -17,14 +21,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductService {
 	private final ProductRepository repo;
+
+	public List<Product> getAllProducts() {
+		return repo.findAll();
+	}
+
+	public List<Product> findByField(String field) {
+		return repo.findAll(Sort.by(Sort.Direction.ASC,field));
+	}
+
+	public Page<Product> findProductBySize(int offset, int pageSize) {
+		return repo.findAll(PageRequest.of(offset, pageSize));
+			
+	}
+
+	public Page<Product> findPaginationWithSort(int offset, int pageSize, String field) {
+		return repo.findAll(PageRequest.of(offset, pageSize).withSort(Sort.by(field)));
+	}
 	
-	@PostConstruct
+/*	@PostConstruct
 	public void initDB() {
 		Random random = new Random();
 		List<Product> products = IntStream.rangeClosed(1, 30)
 				.mapToObj(i -> new Product("product" + i, random.nextInt(100), random.nextInt(5000)))
 				.collect(Collectors.toList());
 		repo.saveAll(products);
-	}
+	}*/
 
 }
